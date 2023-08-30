eabfff896685c16d362df79a1e930428d7b309f4
Animesh404 <akjha6680@gmail.com>
2023-08-30 20:56:50 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+
+import { BrowserRouter as Router, Route, Routes, NavLink} from 'react-router-dom'
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
+import AboutPage from './pages/AboutPage'
+import { FeedbackProvider } from './context/FeedbackContext'
+import AboutIconLink from './components/AboutIconLink'
+
+
+const App = () => {
+    return (
+        <FeedbackProvider>
+            <Router>
+            <Header />
+            <div className="container"> 
+            <Routes>
+            <Route exact path='/' element={
+                <>
+                    <FeedbackForm />
+                    <FeedbackStats />
+                    <FeedbackList />
+                </>
+            }>
+                
+
+                {/* <Route path='/about'>This is the about route</Route> */}
+                </Route>
+
+                <Route path='/about' element={<AboutPage />}/>
+            </Routes>
+
+    
+
+            <AboutIconLink />
+            </div>
+            </Router> 
+        </FeedbackProvider>
+        
+    )
+}
+export default App
+
+// <>fragments(empty brackets)</>
+// can't use class as its a reserved word instead className
+// <label htmlFor=""></label>
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,21 @@
+const FeedbackData = [
+    {
+    
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, voluptas doloremque. Reprehenderit voluptates incidunt ea vero nam repellendus quo '
+    
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, voluptas doloremque. Reprehenderit voluptates incidunt ea vero nam repellendus quo '
+    },
+    {
+        id: 3,
+        rating: 7,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, voluptas doloremque. Reprehenderit voluptates incidunt ea vero nam repellendus quo '
+    }
+]
+
+export default FeedbackData
